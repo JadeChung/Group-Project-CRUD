e111c1fc80a19c34cc6f96b2ff0ea9e323a0a91d
SDK <s.danyal.k@gmail.com>
2021-08-25 19:25:32 -0700

    made changes to db schema

--- Elementary_db.sql
+++ Elementary_db.sql
@@ -1,11 +1,13 @@
 DROP DATABASE IF EXISTS elementary_db;
 CREATE DATABASE elementary_db;
 USE elementary_db;
+
 DROP TABLE IF EXISTS grade_level;
 DROP TABLE IF EXISTS emergency_contact;
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS job_title;
 DROP TABLE IF EXISTS teacher; 
+
 CREATE TABLE teacher (
 teacher_id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(50),
@@ -13,6 +15,7 @@ last_name VARCHAR(100),
 room_number INT,
 PRIMARY KEY (teacher_id)
 );
+
 CREATE TABLE job_title (
 job_id INT NOT NULL AUTO_INCREMENT,
 teacher_id INT,
@@ -22,15 +25,17 @@ end_date VARCHAR(15),
 PRIMARY KEY (job_id),
 FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
 );
+
 CREATE TABLE student (
 student_id INT NOT NULL AUTO_INCREMENT,
 teacher_id INT,
-first_name VARCHAR(50),
+first_name VARCHAR(100),
 last_name VARCHAR(100),
 birth_date VARCHAR(15),
 PRIMARY KEY (student_id),
 FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
 );
+
 CREATE TABLE emergency_contact (
 contact_id INT NOT NULL AUTO_INCREMENT,
 student_id INT,
@@ -41,6 +46,7 @@ email VARCHAR(100),
 PRIMARY KEY (contact_id),
 FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
+
 CREATE TABLE grade_level (
 grade_id INT NOT NULL AUTO_INCREMENT,
 student_id INT,
@@ -50,10 +56,3 @@ PRIMARY KEY (grade_id),
 FOREIGN KEY (student_id) REFERENCES student(student_id),
 FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
 );
-
-
-
-
-
-
-
